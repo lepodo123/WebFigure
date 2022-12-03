a1e951f56be4e5db6bc3a1cf22317bc377aeac06
lepodo123 <116814338+lepodo123@users.noreply.github.com>
2022-12-04 00:02:31 +0700

    le

--- quickorder.js
+++ quickorder.js
@@ -1,14 +1,12 @@
 function quickorder_page(){
     var page = document.getElementById('quick_order_page');
-    var img = document.getElementById('img_product');
-    img.
-    var bd = document.body;
-    bd.style.overflowY = "hidden"
+    var bd =document.body;
+    bd.style.overflowY = "hidden";
     page.style.display = 'block';
 }
 function cancel_page(){
     var page = document.getElementById('quick_order_page');
     var bd = document.body;
-    bd.style.overflowY = "auto"
+    bd.style.overflowY = "auto";
     page.style.display = 'none';
 }
